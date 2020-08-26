556e18102a1e0ffbf2ecbb5f08641bd47d11ae14
DevChinchilla <dev.chinchilla@gmail.com>
2020-08-26 22:33:16 +0900

    comiit

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const TODAY_API_ROOT = "https://api.coronamap.live/today.json";
-export const API_ROOT = "https://api.corona-live.com/";
-
+// export const API_ROOT = "https://api.corona-live.com/";
+export const API_ROOT = "https://devchinchilla.github.io/corona-live-api-v2/";
 export const CITY_TD_FLEX = ["0.45", "0.2", "1.3", "1", "0 1 80px"];
 export const DISTRICT_TD_FLEX = ["0.7", "0.25", "1", "1", "0 1 80px"];
 export const CITY_IDS = [...Array(16).keys()];
